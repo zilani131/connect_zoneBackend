50b09ebcf84b83fccbdb13b37b4a8e201a54151e
zilani131 <zilani.cuet@gmail.com>
2022-07-26 23:53:05 +0600

    my first commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 //user: connectzone 
 //password: nLvAgmiHEoo6mhnT;
+const express = require('express');
+const cors = require('cors'); 
+const app=express();
+const port =process.env.PORT||5000;
+app.use(cors());
+app.use(express.json());//middleware
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
