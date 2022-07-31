3f47e7d0888970744788d696957856860418d4c0
muzahid604 <604muzahid@gmail.com>
2022-07-31 21:58:57 +0600

    test

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const cors = require('cors'); 
-const app=express();
-const port =process.env.PORT||5000;
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
 
 // -------------------
 
@@ -18,28 +18,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
   console.log("zilani");
   client.close();
 });*/
-async function run(){
-    try{
-     await client.connect();
-     const newsCollection=client.db("connectzone").collection("news");
-     app.get('/news',async(req,res)=>{
-     const query={};
-     const news =await newsCollection.find(query).toArray();
-     res.send(news);
+async function run() {
+  try {
+    await client.connect();
+    const newsCollection = client.db("connectzone").collection("news");
+    app.get('/news', async (req, res) => {
+      const query = {};
+      const news = await newsCollection.find(query).toArray();
+      res.send(news);
 
-     })
-    }
-    finally{
+    })
+  }
+  finally {
 
-    }
+  }
 }
 run().catch(console.dir);
 app.use(cors());
 app.use(express.json());//middleware
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
